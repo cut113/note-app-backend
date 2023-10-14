d50bd8db6c563a4a792ca9a65f52db1b6d97135d
Huy <97140326+cut113@users.noreply.github.com>
2023-10-14 10:34:30 +0700

    Complete close DB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,32 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+const MONGODB_URI = 'mongodb+srv://admin:o6DqNuldCum6Jvsw@cluster0.xkvaygw.mongodb.net/'
+
+const DATABASE_NAME = 'noteApp'
+
+import {MongoClient, ServerApiVersion} from 'mongodb'
+
+let DatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI , {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+export const CONNECT_DB = async () => {
+        await mongoClientInstance.connect()
+        DatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+    if (!DatabaseInstance) 
+        throw new Error('Call connect first!')
+        return DatabaseInstance 
+}
+
+export const CLOSE_DB = async () => {
+    console.log('cuuuuuu');
+    await mongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,42 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from "express";
+import { CONNECT_DB, GET_DB, CLOSE_DB } from "~/config/mongodb";
+import exitHook from "async-exit-hook";
+const START_SERVER = () => {
+  const app = express();
 
-import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+  const hostname = "localhost";
+  const port = 8017;
 
-const app = express()
+  app.get("/", async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
+    res.end("<h1>Hello World!</h1><hr>");
+  });
 
-const hostname = 'localhost'
-const port = 8017
+  app.listen(port, hostname, () => {
+    console.log(`Hello Thang DB, I am running at http://${hostname}:${port}/`);
+  });
+  exitHook(() => {
+    CLOSE_DB()
+    console.log("Disconnected from MongoDB successfully!");
+  });
+};
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+(async () => {
+  try {
+    console.log("Connecting to MongoDB successfully!");
+    await CONNECT_DB();
+    console.log("Connected to MongoDB successfully!");
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
-})
+// CONNECT_DB()
+// .then(() => console.log('Connected to MongoDB successfully!'))
+// .then(() => START_SERVER())
+// .catch(error => {
+//   console.error(error)
+//   process.exit(0)
+// })
