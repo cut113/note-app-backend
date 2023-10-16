37095e1114b43b965e28af88fac9cc90297947ea
boybun0154 <huyhoang201555@gmail.com>
2023-10-16 17:12:37 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,17 +2,13 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
-    console.log('req.params: ', req.params)
-    console.log('req.query: ', req.query)
-    console.log('req.cookies: ', req.cookies)
+    // console.log('req.body: ', req.body)
+    // console.log('req.params: ', req.params)
+    // console.log('req.query: ', req.query)
+    // console.log('req.cookies: ', req.cookies)
+
     res.status(StatusCodes.CREATED).json({ message: 'Post ok controller' })
-  } catch (error) {
-    console.log(error)
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
