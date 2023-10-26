2582cb670796e2ae17f2c29c63473c66692897c5
Huy <97140326+cut113@users.noreply.github.com>
2023-10-26 10:01:03 +0700

    Fix schema

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -7,8 +7,29 @@ const CARD_COLLECTION_SCHEMA = Joi.object({
   boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   listId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
 
+  cover: Joi.string().optional().allow(null),
   title: Joi.string().required().min(3).max(50).trim().strict(),
   description: Joi.string().optional(),
+
+  memberIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
+  comments: Joi.array().items(
+    Joi.object({
+      userId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+      content: Joi.string().required().trim().strict(),
+      createdAt: Joi.date().timestamp('javascript').default(Date.now)
+    }).default([]),
+    
+  ).default([]),
+  attachments: Joi.array().items(
+    Joi.object({
+      url: Joi.string().required().trim().strict(),
+      name: Joi.string().required().trim().strict()
+    })
+  ).default([]),
+  startDate: Joi.date().timestamp('javascript').default(null),
+  endDate: Joi.date().timestamp('javascript').default(null), 
 
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
