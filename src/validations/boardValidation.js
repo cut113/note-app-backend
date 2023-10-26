ec794c1fc1be7912bb9d2429427b584636db1d1f
Huy <97140326+cut113@users.noreply.github.com>
2023-10-26 10:41:56 +0700

    Fix add Boards and Validation

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
-
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict().messages({
@@ -12,7 +12,15 @@ const createNew = async (req, res, next) => {
       'string.trim': 'Title cannot contain whitespace',
       'any.required': 'Title is a required field'
     }),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().required().valid('Public', 'Private').default('Public').trim().strict(),
+    ownerId: Joi.string().required(),
+    memberIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    ).default([]),
+    listOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    ).default([]),
   })
 
   try {
