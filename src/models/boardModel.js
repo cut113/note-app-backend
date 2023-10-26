85f1747f652101671f3c6d0019a61691ba3276f1
Huy <97140326+cut113@users.noreply.github.com>
2023-10-26 10:05:17 +0700

    Fix schema

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -4,14 +4,19 @@ import { GET_DB } from '~/config/mongodb'
 
 const BOARD_COLLECTION_NAME = 'board'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
-  title: Joi.string().required().min(3).max(50).trim().strict(),
-  slug: Joi.string().required().min(3).trim().strict(),
-  description: Joi.string().required().min(3).max(256).trim().strict(),
+    title: Joi.string().required().min(3).max(50).trim().strict(),
+    slug: Joi.string().required().min(3).trim().strict(),
+    description: Joi.string().required().min(3).max(256).trim().strict(), 
+    type: Joi.string().required().valid('Public', 'Private').default('Public').trim().strict(),
 
- 
-  listOrderIds: Joi.array().items(
+    ownerId: Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE).required(),
+    memberIds: Joi.array().items(
     Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
-  ).default([]),
+    ).default([]),
+    
+    listOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    ).default([]),
 
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
